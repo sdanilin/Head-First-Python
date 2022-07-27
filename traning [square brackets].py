7b31c2cc046128c15ef1fc709575b5213ae48718
s.danilin <danilinekb@gmail.com>
2022-07-27 17:50:16 +0300

    traning: copy list; [squre brackets]

---
+++ traning [square brackets].py
@@ -0,0 +1,39 @@
+# '''tarning to copy list'''
+# '''Looks like a copy but it's not'''
+# first = [1, 2, 3, 4, 5]
+# print(first)
+# second = first
+# print(second)
+# second.append(6) #erroe
+# print(second)
+# print(first) #error
+#
+# """How to copy the data strukture"""
+# third = second.copy()
+# print(third)
+# third.append(7) # GOOD - new object add to list "third", but not to "first" and "second"
+# print(third)
+# print(second)
+
+
+'''traning [square brackets]'''
+
+saying = "Don't panic!"
+letters = list(saying)
+# print(letters)
+# print(letters[0])
+# print(letters[3])
+# print(letters[6])
+# print(letters[-1])
+# print(letters[-3])
+# print(letters[-6])
+# first = letters[0] #first object on list
+# last = letters[-1] #last oject on list
+# print(first)
+# print(last)
+
+'''letters[start:stop:step] if start is empty = default = 0; if stop is empty = default = max index in list; step default = 1'''
+print(letters[0:10:3]) #except index 10
+print(letters[3:]) #show all letters except firts three
+print(letters[:10]) #show all letters befour (except) index 10
+print(letters[::2]) #every second letter
