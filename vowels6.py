4715b0964f89693aceb0f22c58a3eb1090a9c5bb
s.danilin <danilinekb@gmail.com>
2022-08-02 13:06:02 +0300

    setdefault

--- vowels6.py
+++ vowels6.py
@@ -1,4 +1,3 @@
-'''With KeyError'''
 vowels = ['a', 'e', 'i', 'o', 'u']
 word = input("provide a world to search for vowels:") #сообщает интерпритатору о необходимости запросить у пользователя строку для поиска глассных (выводит поле для воода слова, в котором будеит производиться поиск гласных)
 
@@ -6,6 +5,7 @@ found = {} # создать пустой словарь
 
 for letter in word:
     if letter in vowels:
+        found.setdefault(letter, 0)
         found[letter] += 1 # увеличить значение "found[letter]" на единицу
 
 for k, v in sorted(found.items()): # определить две переменные цикла k  v. Вызвать метод items словоря found, чтоб получить доступ к каждой записи с данными во время итерациий
