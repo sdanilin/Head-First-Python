5a31e38616cd1a7df4171b7821837be68901e519
s.danilin <danilinekb@gmail.com>
2022-08-08 14:52:38 +0300

    tuple

--- vowels7.py
+++ vowels7.py
@@ -2,6 +2,6 @@ vowels = set('aeiou')
 word = input("provide a world to search for vowels:") #сообщает интерпритатору о необходимости запросить у пользователя строку для поиска глассных (выводит поле для воода слова, в котором будеит производиться поиск гласных)
 # i = vowels.intersection(set(word))
 # print(i)
-found = vowels.intersection(set(word))
+found = vowels.intersection(set(word)) # intersection сообщил нам, что в переменной word содержаться глассные. Множество i включает все объекты их vowels7 которые так же присутствуют в set(word)
 for vowel in found:
     print(vowel)
