e51cf91dd7bdfb4ceec5db1725615cad49ed5b56
s.danilin <danilinekb@gmail.com>
2022-08-09 15:01:46 +0300

    def search4vowels(word)

--- vsearch.py
+++ vsearch.py
@@ -1,8 +1,7 @@
-def search4vowels():
-    """Выводит гласные, найденные во введенном словею"""
+def search4vowels(word):
+    """Выводит гласные, найденные в указанном слове."""
     vowels = set('aeiou')
-    word = input('provide a world to search for vowels:')  # сообщает интерпритатору о необходимости запросить у пользователя строку для поиска глассных (выводит поле для воода слова, в котором будеит производиться поиск гласных)
     found = vowels.intersection(set(word))  # intersection сообщил нам, что в переменной word содержаться глассные. Множество i включает все объекты их vowels7 которые так же присутствуют в set(word)
     for vowel in found:
         print(vowel)
-print(search4vowels())
+print(search4vowels('hitch-hiker'))
