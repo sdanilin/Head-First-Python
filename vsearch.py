7171254e834f9b70263012d83fed96b40a5e643a
s.danilin <danilinekb@gmail.com>
2022-08-15 16:57:28 +0300

    word to phrase

--- vsearch.py
+++ vsearch.py
@@ -1,7 +1,7 @@
-def search4vowels(word:str) -> set: #в аннотации указали, что в аргументе word ожидается строка. Указали, что функция возвращает множество
+def search4vowels(phrase:str) -> set: #в аннотации указали, что в аргументе word ожидается строка. Указали, что функция возвращает множество
     """Возвращает, гласные, найденные в указанном слове"""
     vowels = set('aeiou')
-    return vowels.intersection(set(word))  #возврат данных без использования переменной found
+    return vowels.intersection(set(phrase))  #возврат данных без использования переменной found
 print(search4vowels('hitch-hiker'))
 print(search4vowels('galaxy'))
 print(search4vowels('sky'))
