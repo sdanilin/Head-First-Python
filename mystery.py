eeb92c42099558b2395b8cc76542102c29af4346
s.danilin <danilinekb@gmail.com>
2022-08-22 14:06:07 +0300

    Roma vs Julia #1

---
+++ mystery.py
@@ -0,0 +1,28 @@
+def double(arg):
+    print('Befor: ', arg)
+    arg = arg * 2 #Функция удваивает пееданный аргумент
+    print('After: ', arg)
+# num = 10
+# double(num)
+# Befor:  10
+# After:  20
+# num
+# 10
+# saying = 'Hello'
+# double(saying)
+# Befor:  Hello
+# After:  HelloHello
+# saying
+# 'Hello'
+# numbers = [42, 256, 16]
+# double(numbers)
+# Befor:  [42, 256, 16]
+# After:  [42, 256, 16, 42, 256, 16]
+# numbers
+# [42, 256, 16]
+
+
+# def change(arg):
+#     print('Befor: ', arg)
+#     arg.append('More date') #Функция добавляет строку в конец указанного списка
+#     print('After: ', arg)
