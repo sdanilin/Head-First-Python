d2f928f93c1af3b3a684f52a719f372ee769ed56
s.danilin <danilinekb@gmail.com>
2022-08-22 15:11:39 +0300

    merge main with cgapter#4:PEP8

--- mymodules/vsearch.py
+++ mymodules/vsearch.py
@@ -1,10 +1,10 @@
-def search4vowels(phrase:str) -> set: #в аннотации указали, что в аргументе word ожидается строка. Указали, что функция возвращает множество
+def search4vowels(phrase: str) -> set: #в аннотации указали, что в аргументе word ожидается строка. Указали, что функция возвращает множество
     """Возвращает, гласные, найденные в указанном слове"""
     vowels = set('aeiou')
     return vowels.intersection(set(phrase))  #возврат данных без использования переменной found
 
 
-def search4letters(phrase:str, letters:str='aeiou') -> set: #make 'letters' default
+def search4letters(phrase: str, letters: str = 'aeiou') -> set: #make 'letters' default
     """Возвращает множество букв из latters, найденных в указанной фразе"""
     return set(letters).intersection(set(phrase)) #создаем объект множества из letters. найдем пересечение множества, созданного из letters с множеством созданным из phrase. И возвращаем результат вызывающему коду.
 
@@ -18,7 +18,7 @@ def search4letters(phrase:str, letters:str='aeiou') -> set: #make 'letters' defa
 # print(search4letters('galaxy', 'xyz'))
 # print(search4letters('life, the universe, and everything', 'o'))
 
-print(search4letters('life, the universe, and everything'))
-print(search4letters('life, the universe, and everything', 'aeiou'))
-# print(search4vowels('life, the universe, and everything'))
-print(search4letters(letters='xyz', phrase='galaxy')) #порядок аргументов не важен если присваивать их по именам параметров
+# print(search4letters('life, the universe, and everything'))
+# print(search4letters('life, the universe, and everything', 'aeiou'))
+# # print(search4vowels('life, the universe, and everything'))
+# print(search4letters(letters='xyz', phrase='galaxy')) #порядок аргументов не важен если присваивать их по именам параметров
