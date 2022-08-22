9d4e20db38b2dbabb447dc26444be3ed67bc66f1
s.danilin <danilinekb@gmail.com>
2022-08-22 18:22:28 +0300

    Flask_webapp_develop_do_search

--- webapp/hello_flask.py
+++ webapp/hello_flask.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from vsearch import search4letters
 
 app = Flask(__name__) #Создание экземпляра объекта Flask и присваивание его переменной
 
-@app.route('/')
-def hello() -> str:
+@app.route('/') # ('/') - это URL. Декоратор функции (начинается с @)- настраивает поведение существующей функции без изменения кода самой функции
+def hello() -> str: #Функция возвращающая строку
     return 'Hello world from Flask!'
 
-app.run()
+@app.route('/search4') # декоратор устанавливает URL
+def do_search() -> str:
+    return str(search4letters('life, the universe, and everything', 'eiru,!')) #Функция do_search вызывает search4letters и возвращает результат в виде строки
+
+app.run() #Строка предлагает объекту Flask в переменной app запустить веб-сервер, вызывая метод run
