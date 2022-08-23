2edbfd623527cada732c30622082a48a1e43cb04
s.danilin <danilinekb@gmail.com>
2022-08-23 15:54:18 +0300

    method POST; debug

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,4 +1,4 @@
-
+import html
 from flask import Flask, render_template
 from vsearch import search4letters
 
@@ -8,13 +8,13 @@ app = Flask(__name__)
 def hello() -> str:
     return 'Hello world from Flask!'
 
-@app.route('/search4')
+@app.route('/search4', methods=['POST'])
 def do_search() -> str:
     return str(search4letters('life, the univers, and everything', 'eiru,!'))
 
 @app.route('/entry')
-def entry_page() -> str:
-    return render_template('entry_html',
+def entry_page() -> html:
+    return render_template('entry.html',
                            the_title='Welcome tp search4letters on the web')
 
-app.run()
+app.run(debug=True)
