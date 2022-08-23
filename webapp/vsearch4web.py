3f8ebfc3b3c604fec9c1858761c96af2504c2e60
s.danilin <danilinekb@gmail.com>
2022-08-23 18:05:22 +0300

    get results DONE

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -9,14 +9,20 @@ def hello() -> str:
     return 'Hello world from Flask!'
 
 @app.route('/search4', methods=['POST'])
-def do_search() -> str:
+def do_search() -> html:
     phrase = request.form['phrase'] #создаем новые переменные
     letters = request.form['letters'] #и присваиваем им данные из HTML формы
-    return str(search4letters(phrase, letters)) #используем эти переменные в вызове "search4letters"
+    title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html',
+                            the_phrase=phrase,
+                            the_letters=letters,
+                            the_title=title,
+                            the_results=results,)
 
 @app.route('/entry')
 def entry_page() -> html:
     return render_template('entry.html',
-                           the_title='Welcome tp search4letters on the web')
+                           the_title='Welcome tp search4letters on the web!')
 
 app.run(debug=True)
